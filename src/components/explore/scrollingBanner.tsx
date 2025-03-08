5e28d569931ef7adea727cbf5e21926bd8374053
unknown <helendeee12@gmail.com>
2025-03-08 22:46:27 +0100

    Add: Implemented scrolling banner

--- src/components/explore/scrollingBanner.tsx
+++ src/components/explore/scrollingBanner.tsx
@@ -1,9 +1,16 @@
-import React from 'react'
-import Image from 'next/image'
+"use client";
+
+import React from 'react';
+import Image, { StaticImageData } from 'next/image';
 import brownBag from '../../../public/brown-bag.png';
 import blueBag from '../../../public/blue-bag.png';
 
-const scrollingText = [
+interface ScrollingItem {
+    img: StaticImageData;
+    text: string;
+}
+
+const scrollingText: ScrollingItem[] = [
     {
         img: brownBag,
         text: 'Get 20% off coupon "BAG20"',
@@ -12,40 +19,54 @@ const scrollingText = [
         img: blueBag,
         text: 'Get 20% off coupon "BAG20"',
     }
-]
+];
 
-const ScrollingBanner = () => {
+const ScrollingBanner: React.FC = () => {
   return (
     <section className='mx-4 my-8 rounded-xl'>
-        <div className='relative overflow-hidden bg-[#f4f4f4] py-4 rounded-xl'>
-            {/* right duplicate scrolling line */}
-            <div className="flex whitespace-nowrap animate-scroll-right">
-                {Array(3).fill(scrollingText).flat().map((item, index) => (
-                    <div key={`line1-${index}`} className='flex items-center mx-4 '>   
-                        <Image src={item.img} alt="bag-img" className='w-12 h-auto'/>
-                        <span className='uppercase text-2xl  p-2 text-gray-400'>
-                        {item.text}
-                        </span>
-                    </div>
-            ))}
+      <div className='relative overflow-hidden bg-[#f4f4f4] py-4 rounded-xl'>
+        {/* Right to left scrolling line */}
+        <div className="flex whitespace-nowrap animate-scroll-left-slow">
+          {Array(3).fill(scrollingText).flat().map((item, index) => (
+            <div key={`line1-${index}`} className='flex items-center mx-4'>
+              <Image src={item.img} alt="bag-img" className='w-12 h-auto'/>
+              <span className='uppercase text-2xl p-2 text-gray-400'>{item.text}</span>
             </div>
-
-        {/* left duplicate scrolling line */}
-        <div className="flex whitespace-nowrap animate-scroll-left">
-                {Array(3).fill(scrollingText).flat().map((item, index) => (
-                    <div key={`line2-${index}`} className='flex items-center mx-4 '>   
-                        
-                        
-                        <Image src={item.img} alt="bag-img" className='w-12 h-auto '/>
-                        <span className='uppercase text-2xl  p-2 text-gray-400'>
-                        {item.text}
-                        </span>
-                    </div>
-            ))}
-            </div>
+          ))}
         </div>
-    </section>
-  )
-}
 
-export default ScrollingBanner
+        {/* Left to right scrolling line */}
+        <div className="flex whitespace-nowrap animate-scroll-right-slow mt-4">
+          {Array(3).fill(scrollingText).flat().map((item, index) => (
+            <div key={`line2-${index}`} className='flex items-center mx-4'>
+              <Image src={item.img} alt="bag-img" className='w-12 h-auto'/>
+              <span className='uppercase text-2xl p-2 text-gray-400'>{item.text}</span>
+            </div>
+          ))}
+        </div>
+      </div>
+
+      <style jsx>{`
+        @keyframes scroll-left-slow {
+          from { transform: translateX(100%); }
+          to { transform: translateX(-100%); }
+        }
+
+        @keyframes scroll-right-slow {
+          from { transform: translateX(-100%); }
+          to { transform: translateX(100%); }
+        }
+
+        .animate-scroll-left-slow {
+          animation: scroll-left-slow 20s linear infinite;
+        }
+
+        .animate-scroll-right-slow {
+          animation: scroll-right-slow 20s linear infinite;
+        }
+      `}</style>
+    </section>
+  );
+};
+
+export default ScrollingBanner;
